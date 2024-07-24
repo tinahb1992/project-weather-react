7923cd33407b800678ef6f1606e21bce3b432d8b
tinahb1992 <dmmatshwaane@gmail.com>
2024-07-24 11:09:22 +0200

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.dinah.io/" target="_blank">
+          Dinah Buthane
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/tinahb1992/project-weather-react"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
