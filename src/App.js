b27fbf38e88251ae8b42220c16f19b6e08df252e
tinahb1992 <dmmatshwaane@gmail.com>
2024-07-24 13:11:22 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.dinah.io/" target="_blank">
-          Dinah Buthane
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/tinahb1992/project-weather-react"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.dinah.io/" target="_blank">
+            Dinah Buthane
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/tinahb1992/project-weather-react"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
