5ae86a9771835289a3068024c81bb35fe2751287
tinahb1992 <dmmatshwaane@gmail.com>
2024-07-26 08:39:02 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,54 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <div className="weather">
+          <form>
+            <div className="row">
+              <div className="col-7">
+                <input
+                  type="search"
+                  placeholder="Enter a city.."
+                  className="form-control"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-2">
+                <input
+                  type="submit"
+                  value="search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <h1>New York</h1>
+          <ul>
+            <li>Wednesday 07:00</li>
+            <li>Mostly Cloudy</li>
+          </ul>
+          <div className="row mt-3">
+            <div className="col-6">
+              <div className="clearfix">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Mostly Cloudy"
+                  className="float-left"
+                />
+                <span className="float-left">
+                  <span className="temperature">6</span>
+                  <span className="unit">&deg;C</span>
+                </span>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Precipitation: 15%</li>
+                <li>Humidity: 72%</li>
+                <li>Wind: 13km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
         <footer>
           This project was coded by{" "}
           <a href="https://www.dinah.io/" target="_blank">
